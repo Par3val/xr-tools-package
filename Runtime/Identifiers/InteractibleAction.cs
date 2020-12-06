c9475ab78be9fc044c02716fbbb25bdfa5bb1427
Par3val <zale@323@comcast.net>
2020-12-06 01:09:26 -0600

    Missed a script in last commit

---
+++ Runtime/Identifiers/InteractibleAction.cs
@@ -0,0 +1,7 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class InteractibleAction : MonoBehaviour
+{
+	public InteractibleObject.SecondaryTypes type;
+}
